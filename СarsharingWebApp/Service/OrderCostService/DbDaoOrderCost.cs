3f63419c61c2d2fe8555c03bb18a5132ae6ec6fe
Roman Yakushev <boxboxbox@inbox.ru>
2023-04-22 15:05:12 +0300

    Finish All DbDao

---
+++ СarsharingWebApp/Service/OrderCostService/DbDaoOrderCost.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApiApp.Service.ClientService;
+using СarsharingWebApp.Model;
+using СarsharingWebApp.Model.Entity;
+
+namespace СarsharingWebApp.Service.OrderCostService
+{
+    public class DbDaoOrderCost : IDaoOrderCost
+    {
+        private static List<OrderCost> _orderCost = new List<OrderCost>();
+        private static int currentId = 1;
+        private ApplicationDbContext db;
+
+        public DbDaoOrderCost(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+
+        public async Task<OrderCost> AddAsync(OrderCost orderCost)
+        {
+            await db.AddAsync(orderCost);
+            await db.SaveChangesAsync();
+            return orderCost;
+        }
+
+        public async Task<OrderCost> DeleteAsync(OrderCost orderCost)
+        {
+            if (await db.OrderCost.FirstOrDefaultAsync() != null)
+            {
+                db.OrderCost.Remove(orderCost);
+                await db.SaveChangesAsync();
+            }
+            return orderCost;
+        }
+
+        public Task<List<OrderCost>> GetAllAsync()
+        {
+            return db.OrderCost.ToListAsync();
+        }
+
+        public async Task<OrderCost> GetAsync(OrderCost orderCost)
+        {
+            return await db.OrderCost.FirstOrDefaultAsync(p => p.Id == orderCost.Id);
+        }
+
+        public async Task<OrderCost> UpdateAsync(OrderCost orderCost)
+        {
+            if (await db.OrderCost.FirstOrDefaultAsync() != null)
+            {
+                db.OrderCost.Update(orderCost);
+                await db.SaveChangesAsync();
+                return orderCost;
+            }
+            return orderCost;
+        }
+    }
+}
---
+++ СarsharingWebApp/Service/PersonService/DbDaoPerson.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApiApp.Service.ClientService;
+using СarsharingWebApp.Model;
+using СarsharingWebApp.Model.Entity;
+
+namespace СarsharingWebApp.Service.PersonService
+{
+    public class DbDaoPerson : IDaoPerson
+    {
+        private static List<Person> _person = new List<Person>();
+        private static int currentId = 1;
+        private ApplicationDbContext db;
+
+        public DbDaoPerson(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+
+        public async Task<Person> AddAsync(Person person)
+        {
+            await db.AddAsync(person);
+            await db.SaveChangesAsync();
+            return person;
+        }
+
+        public async Task<Person> DeleteAsync(Person person)
+        {
+            if (await db.Person.FirstOrDefaultAsync() != null)
+            {
+                db.Person.Remove(person);
+                await db.SaveChangesAsync();
+            }
+            return person;
+        }
+
+        public Task<List<Person>> GetAllAsync()
+        {
+            return db.Person.ToListAsync();
+        }
+
+        public async Task<Person> GetAsync(Person person)
+        {
+            return await db.Person.FirstOrDefaultAsync(p => p.Id == person.Id);
+        }
+
+        public async Task<Person> UpdateAsync(Person person)
+        {
+            if (await db.Client.FirstOrDefaultAsync() != null)
+            {
+                db.Person.Update(person);
+                await db.SaveChangesAsync();
+                return person;
+            }
+            return person;
+        }
+    }
+}
